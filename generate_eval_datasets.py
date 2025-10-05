15ccab800860fe6820a751fda50364fd78dfebc4
marme <marcel.1710@live.de>
2025-10-05 18:49:52 +0200

    add finetuning code

--- generate_eval_datasets.py
+++ generate_eval_datasets.py
@@ -2,102 +2,39 @@ from operator import itemgetter
 from collections import defaultdict
 import numpy as np
 import pandas as pd
-from datasets import Dataset
+from datasets import Dataset, IterableDataset, load_from_disk
+from tqdm import tqdm
 
 
-def dataset_refined_bookcorpus():
-    from datasets import load_from_disk
+def dataset_dict_gen(path):
+    ds = load_from_disk(path)
+    ids = ds.to_iterable_dataset()
 
-    ds = load_from_disk("data/refined-bookcorpus-dataset_hf_test")
+    for datum_dict in tqdm(ids, total=len(ds)): #ds["text"]:
+        paragraphs = list(filter(len, datum_dict["text"].split("\n\n"))) #list(filter(len, datum_dict["text"].split(".\n\n")))
 
-    all_paragraphs = []
-    for tokens, ner_tags in zip(ds["tokens"], ds["ner_tags"]):
-        prev_tag_index = 0
+        tokens = []
+        ner_tags = []
 
-        for tag_index in np.nonzero(ner_tags)[0]:
-            tag_index += 1
-            para = " ".join(tokens[prev_tag_index:tag_index])
-            all_paragraphs.append(para)
+        for para in paragraphs:
+            for token in para.split(" "):
+                tokens.append(token)
+                ner_tags.append(0)
+            ner_tags[-1] = 1
 
-            prev_tag_index = tag_index
+        yield {"tokens": tokens, "ner_tags": ner_tags}
 
-    return all_paragraphs
-
-
-def dataset_MultiLegalSBD_en_judgements():
-    filenames = [
-        "data/MultiLegalSBD/data/en_judgements_0.jsonl",
-        "data/MultiLegalSBD/data/en_judgements_1.jsonl",
-        "data/MultiLegalSBD/data/en_judgements_2.jsonl",
-        "data/MultiLegalSBD/data/en_judgements_3.jsonl",
-    ]
-
-    dfs = []
-    for filename in filenames:
-        dfs.append(pd.read_json(filename, lines=True))
-
-    df = pd.concat(dfs)
-
-    all_paragraphs = []
-
-    for text in df.text.values:
-        paragraphs = list(filter(len, text.split("\r\n")))
-
-        all_paragraphs.extend(paragraphs)
-
-    return all_paragraphs
-
-
-def hf_dataset_with_paragraphs(dataset_id, split):
-    from datasets import load_dataset
-
-    ds = load_dataset(dataset_id)
-
-    all_paragraphs = []
-
-    for text in ds[split]["text"]:
-        paragraphs = list(filter(len, text.split("\n")))
-
-        all_paragraphs.extend(paragraphs)
-
-    return all_paragraphs
-
-
-def to_dict(paragraphs):
-    tokens = []
-    ner_tags = []
-
-    for para in paragraphs:
-        for token in para.split(" "):
-            tokens.append(token)
-            ner_tags.append(0)
-
-        ner_tags[-1] = 1
-
-    assert len(tokens) == len(ner_tags)
-
-    return {"tokens": tokens, "ner_tags": ner_tags}
 
 
 def main():
     raw_datasets = [
-        ("bookcorpus", dataset_refined_bookcorpus()),
-        ("en_judgements", dataset_MultiLegalSBD_en_judgements()),
-        (
-            "paul_graham",
-            hf_dataset_with_paragraphs(
-                dataset_id="sgoel9/paul_graham_essays", split="train"
-            ),
-        ),
-        (
-            "20_newsgroups",
-            hf_dataset_with_paragraphs(dataset_id="SetFit/20_newsgroups", split="test"),
-        ),
+            "data/en_wikipedia_2023_train",
+            "data/en_wikipedia_2023_validation",
     ]
 
-    for dataset_name, paragraphs in raw_datasets:
-        dataset = Dataset.from_dict(to_dict(paragraphs))
-        dataset.save_to_disk(f"data/{dataset_name}")
+    for dataset_name in raw_datasets:
+        dataset = Dataset.from_generator(dataset_dict_gen, gen_kwargs={"path": dataset_name})
+        dataset.save_to_disk(f"{dataset_name}_processed")
 
 
 if __name__ == "__main__":
--- train.py
+++ train.py
@@ -1,7 +1,7 @@
 import numpy as np
 import torch
 import evaluate
-from datasets import load_from_disk
+from datasets import load_from_disk, load_dataset
 from transformers import (
     AutoTokenizer,
     AutoModelForTokenClassification,
@@ -56,24 +56,27 @@ def modernbert(model_id):
 
 
 def main(dataset_id, model_id, output_dir, batch_size, max_seq_len=None):
-    model, tokenizer = modernbert(model_id=model_id)
-    # model, tokenizer = distilbert(model_name=model_id)
+    #model, tokenizer = modernbert(model_id=model_id)
+    model, tokenizer = distilbert(model_name=model_id)
 
     if max_seq_len is None:
         max_seq_len = tokenizer.model_max_length
 
-    dataset = load_from_disk(dataset_id)
+    dataset = load_dataset(dataset_id)
     print(dataset)
-    dataset_val = dataset["test"]
+    dataset_val = dataset["validation"]
+    dataset_val = dataset_val.shuffle(seed=42)
+    dataset_val = dataset_val.select(range(10000))
     dataset_train = dataset["train"]
+    del dataset
     label_list = ["O", "separator"]
     seqeval = evaluate.load("seqeval")
 
     def tokenize_and_align_labels(examples):
         tokenized_inputs = tokenizer(
             examples["tokens"],
-            truncation=True,
-            is_split_into_words=True,
+            truncation=True,  # TODO: Is truncating to max_seq_len really the way to do it? https://stackoverflow.com/questions/65246703/how-does-max-length-padding-and-truncation-arguments-work-in-huggingface-bertt#65255500
+            is_split_into_words=True, # https://discuss.huggingface.co/t/token-classification-how-to-tokenize-and-align-labels-with-overflow-and-stride/4353/4
             max_length=max_seq_len,
         )
 
@@ -121,8 +124,10 @@ def main(dataset_id, model_id, output_dir, batch_size, max_seq_len=None):
             "accuracy": results["overall_accuracy"],
         }
 
-    tokenized_dataset_train = dataset_train.map(tokenize_and_align_labels, batched=True)
-    tokenized_dataset_val = dataset_val.map(tokenize_and_align_labels, batched=True)
+    tokenized_dataset_train = dataset_train.map(tokenize_and_align_labels, batched=True, num_proc=16)
+    del dataset_train
+    tokenized_dataset_val = dataset_val.map(tokenize_and_align_labels, batched=True, num_proc=16)
+    del dataset_val
 
     data_collator = DataCollatorForTokenClassification(tokenizer=tokenizer)
     training_args = TrainingArguments(
@@ -130,13 +135,20 @@ def main(dataset_id, model_id, output_dir, batch_size, max_seq_len=None):
         learning_rate=2e-5,
         per_device_train_batch_size=batch_size,
         per_device_eval_batch_size=batch_size,
-        num_train_epochs=25,
+        #num_train_epochs=1,
+        max_steps=18000,
         weight_decay=0.01,
-        eval_strategy="epoch",
-        save_strategy="epoch",
+        eval_strategy="steps",
+        save_strategy="steps",
+        save_steps=1000,
         load_best_model_at_end=True,
-        push_to_hub=False,
+        push_to_hub=True,
+        hub_strategy="checkpoint",
+        hub_token="",
         fp16=True,
+        eval_on_start=True,
+        save_total_limit=5,
+        metric_for_best_model="f1"
     )
 
     trainer = Trainer(
@@ -154,9 +166,9 @@ def main(dataset_id, model_id, output_dir, batch_size, max_seq_len=None):
 
 if __name__ == "__main__":
     main(
-        dataset_id="data/refined-bookcorpus-dataset_hf_ModernBERT-base1024",
-        model_id="modernbert_large_minipile/checkpoint-54717",
-        output_dir="modernbert_large_minipile_plus_bookcorpus",
+        dataset_id="mamei16/wikipedia_paragraphs_token_classification",
+        model_id="mirth/chonky_distilbert_base_uncased_1",
+        output_dir="chonky_distilbert_base_uncased_1.1",
         batch_size=64,
-        max_seq_len=1024,
+        max_seq_len=512,
     )

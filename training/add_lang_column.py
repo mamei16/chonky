f48cd868ab7165e6a513b2821da0f1463d5135c4
marme <marcel.1710@live.de>
2025-11-14 13:03:46 +0100

    put training scripts into separate directory

--- training/add_lang_column.py
+++ training/add_lang_column.py
@@ -25,7 +25,7 @@ for name in dataset_names:
     ds = load_dataset("data/wikipedia_shuffled/" + "20231101." + name)
     print(name)
     ds["train"] = ds["train"].add_column(name="lang", column=[name]*len(ds["train"]))
-    ds.save_to_disk("data/wikipedia_shuffled_filtered/" + "20231101." + name, num_proc=16)
+    ds.save_to_disk(Path("data/wikipedia_shuffled_filtered/" + "20231101." + name), num_proc=16)
     rmdir(Path.home() / ".cache/huggingface/datasets" / ("20231101." + name))
     rmdir(Path("data/wikipedia_shuffled/" + "20231101." + name))
     print()
